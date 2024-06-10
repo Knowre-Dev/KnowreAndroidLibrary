dd8003d7ece50c614c5839d9a7cfa61aace185ab
klokov <dnwjdgus1994@gmail.com>
2024-06-10 13:45:02 +0900

    Update: SpecBasedReducer.invoke final

--- extension-standard/src/main/java/com/knowre/android/extension/standard/redux/SpecBasedReducer.kt
+++ extension-standard/src/main/java/com/knowre/android/extension/standard/redux/SpecBasedReducer.kt
@@ -7,7 +7,7 @@ open class SpecBasedReducer<R : Reducible, Spec : ReduceSpec<R>>(
     protected val spec: (R) -> Spec
 ) : (R, ReduceAction<R, Spec>) -> R {
 
-    override fun invoke(reducible: R, reduceAction: ReduceAction<R, Spec>): R =
+    final override operator fun invoke(reducible: R, reduceAction: ReduceAction<R, Spec>): R =
         reduce(reducible, reduceAction)
 
     private fun reduce(snapshot: R, reduceAction: ReduceAction<R, Spec>): R =
